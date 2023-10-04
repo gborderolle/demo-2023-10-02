d3f4462f4e51b888cc4c892eda6b4f6ac0c2f110
Gonzalo Borderolle <gborderolle@gmail.com>
2023-10-04 11:13:16 -0300

    .

--- src/components/Layout/Contenedor.js
+++ src/components/Layout/Contenedor.js
@@ -9,9 +9,9 @@ function ContenedorLayout(props) {
           <div className='card-body'>
             <h5 className='card-title'>{props.title}</h5>
             <h6 className='card-subtitle mb-2 text-muted'>{props.subtitle}</h6>
-            <p className='card-text'>
+            <div className='card-text'>
               <Outlet />
-            </p>
+            </div>
           </div>
         </div>
       </div>
--- src/components/pages/Setup.js
+++ src/components/pages/Setup.js
@@ -1,36 +1,84 @@
 import { v4 as uuidv4 } from 'uuid';
+
 import ErrorMessage from '../UI/ErrorMessage';
 import useInput from '../../hooks/use-input';
 
 const Setup = (props) => {
   const {
-    value: enteredName,
-    isValid: enteredNameIsValid,
-    hasError: nameInputHasError,
-    valueChangeHandler: nameInputChangeHandler,
-    inputBlurHandler: nameInputBlurHandler,
-    reset: resetNameInput,
+    value: enteredName1,
+    isValid: enteredNameIsValid1,
+    hasError: nameInputHasError1,
+    valueChangeHandler: nameInputChangeHandler1,
+    inputBlurHandler: nameInputBlurHandler1,
+    reset: resetNameInput1,
   } = useInput((value) => value.trim() !== '');
 
-  const formSubmitHandler = (event) => {
+  const {
+    value: enteredName2,
+    isValid: enteredNameIsValid2,
+    hasError: nameInputHasError2,
+    valueChangeHandler: nameInputChangeHandler2,
+    inputBlurHandler: nameInputBlurHandler2,
+    reset: resetNameInput2,
+  } = useInput((value) => value.trim() !== '');
+
+  //  -------------------------------------------------
+
+  const formSubmitHandler1 = (event) => {
     event.preventDefault();
 
-    if (!enteredNameIsValid) {
+    if (!enteredNameIsValid1) {
       return;
     }
-
-    loadDummy();
-    resetNameInput();
+    
+    loadDummy1();
+    resetNameInput1();
   };
 
-  const nameInputClasses = nameInputHasError
+  const nameInputClasses1 = nameInputHasError1
     ? 'form-control is-invalid'
     : 'form-control';
 
-  const loadDummy = async () => {
+  const loadDummy1 = async () => {
+    const newItem = {
+      partyId: uuidv4(),
+      partyName: enteredName1,
+    };
+
+    let id = uuidv4();
+    await fetch(
+      'https://react-http-23a93-default-rtdb.firebaseio.com/partyList.json',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          partyId: uuidv4(),
+          partyName: enteredName1,
+        }),
+      }
+    );
+  };
+
+  //  -------------------------------------------------
+
+  const formSubmitHandler2 = (event) => {
+    event.preventDefault();
+
+    if (!enteredNameIsValid2) {
+      return;
+    }
+
+    loadDummy2();
+    resetNameInput2();
+  };
+
+  const nameInputClasses2 = nameInputHasError2
+    ? 'form-control is-invalid'
+    : 'form-control';
+
+  const loadDummy2 = async () => {
     const newItem = {
       partidoId: uuidv4(),
-      partidoName: enteredName,
+      partidoName: enteredName2,
       partidoVotos: 0,
     };
 
@@ -41,7 +89,7 @@ const Setup = (props) => {
         method: 'POST',
         body: JSON.stringify({
           partidoId: uuidv4(),
-          partidoName: enteredName,
+          partidoName: enteredName2,
           partidoVotos: 0,
         }),
       }
@@ -49,39 +97,69 @@ const Setup = (props) => {
   };
 
   return (
-    <div className='container mt-5' style={{ maxWidth: '500px' }}>
-      <div className='card'>
-        <div className='card-body'>
-          <form onSubmit={formSubmitHandler}>
-            <div className='form-group d-flex align-items-center justify-content-between mb-3'>
-              <label htmlFor='input' className='me-3'>
-                Lista
-              </label>
-              <input
-                type='text'
-                id='input'
-                className={nameInputClasses}
-                value={enteredName}
-                onChange={nameInputChangeHandler}
-                onBlur={nameInputBlurHandler}
-                style={{ maxWidth: '200px' }}
-              />
-            </div>
-            {nameInputHasError && (
-              <div className='text-danger mb-3'>Cantidad inválida.</div>
-            )}
-            <div className='mb-3'>
-              <button type='submit' className='btn btn-primary'>
-                Confirmar
-              </button>
-            </div>
-          </form>
+    <>
+      <div className='container mt-5' style={{ maxWidth: '500px' }}>
+        <div className='card'>
+          <div className='card-body'>
+            <form onSubmit={formSubmitHandler1}>
+              <div className='form-group d-flex align-items-center justify-content-between mb-3'>
+                <label htmlFor='input' className='me-3'>
+                  Partido nuevo
+                </label>
+                <input
+                  type='text'
+                  id='input'
+                  className={nameInputClasses1}
+                  value={enteredName1}
+                  onChange={nameInputChangeHandler1}
+                  onBlur={nameInputBlurHandler1}
+                  style={{ maxWidth: '200px' }}
+                />
+              </div>
+              {nameInputHasError1 && (
+                <div className='text-danger mb-3'>Cantidad inválida.</div>
+              )}
+              <div className='mb-3'>
+                <button type='submit' className='btn btn-primary'>
+                  Confirmar
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-      {!enteredNameIsValid && (
-        <ErrorMessage errorMessage='El formulario no es válido.' />
-      )}
-    </div>
+
+      <div className='container mt-5' style={{ maxWidth: '500px' }}>
+        <div className='card'>
+          <div className='card-body'>
+            <form onSubmit={formSubmitHandler2}>
+              <div className='form-group d-flex align-items-center justify-content-between mb-3'>
+                <label htmlFor='input' className='me-3'>
+                  Lista nueva
+                </label>
+                <input
+                  type='text'
+                  id='input'
+                  className={nameInputClasses2}
+                  value={enteredName2}
+                  onChange={nameInputChangeHandler2}
+                  onBlur={nameInputBlurHandler2}
+                  style={{ maxWidth: '200px' }}
+                />
+              </div>
+              {nameInputHasError2 && (
+                <div className='text-danger mb-3'>Cantidad inválida.</div>
+              )}
+              <div className='mb-3'>
+                <button type='submit' className='btn btn-primary'>
+                  Confirmar
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
